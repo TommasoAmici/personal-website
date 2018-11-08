5d53f04448c5bab98d95023e01c3dcd9343efdde
Tommaso A <tommaso.amici@me.com>
2018-11-09 00:15:49 +0100

    set up sass

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+// in gatsby-config.js
+module.exports = {
+  siteMetadata: {
+    title: "Tommaso Amici",
+    description: "My personal website and portfolio",
+    siteUrl: "https://tommasoamici.com",
+    author: "Tommaso Amici"
+  },
+  plugins: [`gatsby-plugin-sass`]
+};
