3f5680f5b77ef010961785836d3764d343c23d3b
Tommaso A <tommaso.amici@me.com>
2018-11-09 00:18:53 +0100

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavLink = props => (
+  <li className="nav-li">
+    <a className="nav-link" onClick={props.onClick}>
+      {props.children}
+    </a>
+  </li>
+);
+
+export default props => {
+  const { clickHome, clickAbout, clickProjects } = props;
+  return (
+    <nav>
+      <ul className="nav-ul">
+        <NavLink onClick={clickHome}>Home</NavLink>
+        <NavLink onClick={clickAbout}>About</NavLink>
+        <NavLink onClick={clickProjects}>Projects</NavLink>
+      </ul>
+    </nav>
+  );
+};
