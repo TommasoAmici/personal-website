49b9f8a664c5943a4e5e7bca47198f92377d4015
Tommaso A <tommaso.amici@me.com>
2018-11-09 04:43:08 +0100

    template and plugins to read markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,32 @@ module.exports = {
     siteUrl: "https://tommasoamici.com",
     author: "Tommaso Amici"
   },
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    }
+  ]
 };
