df14529da4cbd20dc5aeddee630b05637f4a09cc
Tommaso A <tommaso.amici@me.com>
2018-11-11 01:01:02 +0100

    added parallax background to archive pages

--- src/pages/misc-archive.js
+++ src/pages/misc-archive.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavbarAlt } from "../components/Navbar";
 import TableOfContentsBlog from "../components/TableOfContentsBlog";
+import TableOfContents from "../components/TableOfContents";
+import Background from "../components/SVGsBackground";
 
 export default () => (
   <>
     <NavbarAlt />
-    <div className="table-of-projects">
-      <TableOfContentsBlog all={true} />
-    </div>
+    <Background>
+      <div className="table-of-projects">
+        <TableOfContentsBlog all={true} />
+      </div>
+    </Background>
   </>
 );
--- src/pages/projects-archive.js
+++ src/pages/projects-archive.js
@@ -1,12 +1,15 @@
 import React from "react";
 import TableOfContents from "../components/TableOfContents";
 import { NavbarAlt } from "../components/Navbar";
+import Background from "../components/SVGsBackground";
 
 export default () => (
   <>
     <NavbarAlt />
-    <div className="table-of-projects">
-      <TableOfContents all={true} />
-    </div>
+    <Background>
+      <div className="table-of-projects">
+        <TableOfContents all={true} />
+      </div>
+    </Background>
   </>
 );
